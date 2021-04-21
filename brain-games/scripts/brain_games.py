1f2e56fe79deb64fc85126192dfb3b32a7ba7aae
_artemy <artemyandrienko@gmail.com>
2021-04-22 00:25:32 +0300

    initial commit

---
+++ brain-games/scripts/brain_games.py
@@ -0,0 +1,15 @@
+"""
+    description: Brain games welcome game.
+    author: _artemy
+"""
+
+# from brain_games.engine import run
+
+
+def main():
+    """Welcome user without running a game."""
+    # run()
+    print('Welcome to the Brain Games!')
+
+if __name__ == '__main__':
+    main()
