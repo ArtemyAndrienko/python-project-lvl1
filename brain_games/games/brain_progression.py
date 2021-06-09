f81e3168e0db5610ecf6eb9e7900f53bc44fbaa2
artemy <artemy.andrienko@gmail.com>
2021-06-10 00:07:25 +0300

    fix 0.9.9 - some fixes

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -15,6 +15,7 @@ def make_question():
     step = randint(1, 10)
     missing_index = randint(0, LENGTH - 1)
     progession = [first + (i * step) for i in range(LENGTH)]
-    expression = ' '.join([str(val) if idx != missing_index else '..' for idx, val in enumerate(progession)])
+    expression = ' '.join([str(val) if idx != missing_index else '..'
+                           for idx, val in enumerate(progession)])
     result = str(progession[missing_index])
     return (expression, result)
